553f1f12b50b7acc2a82d2c42ab9e393e6b4d209
Pratik Datt <dattpratik2151@gmail.com>
2024-04-19 19:37:24 +0530

    initial commit first

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base :"First_react_App",
+  base :"/React_Application/",
   plugins: [react()],
 })
